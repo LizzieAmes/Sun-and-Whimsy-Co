36243ee81fa341e131fee1e9f02c165fa751440b
advlia <147446558+advlia@users.noreply.github.com>
2024-04-02 16:09:23 -0600

    inserted payment/shipping details examp.

--- server/models/Order.js
+++ server/models/Order.js
@@ -9,6 +9,15 @@ const orderSchema = new mongoose.Schema({
   orderDate: { type: Date, default: Date.now },
   status: { type: String, default: 'pending' },
   // Include payment confirmation, shipping details, etc.
+
+paymentConfirmation: 'Payment recceived. Transaction ID: 34565174',
+shippingDetails: {
+  address: '752 Main St',
+  city: 'Salt Lake City',
+  //add more details?
+}
+
 });
 
 module.exports = mongoose.model('Order', orderSchema);
+
